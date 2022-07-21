7a130668d6f5f9724ebfc367535eec2fa0933006
yuki-inui <courage8210@gmail.com>
2022-07-21 23:06:23 +0900

    atom

---
+++ pages/atoms/SubscribeButton/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const SubscribeButton = () => {
+  <Buttonfont>Subscribe</Buttonfont>;
+};
+
+const Buttonfont = styled.div`
+  color: black;
+  font-size: 10px;
+  font-weigh: normal;
+`;
+
+export default SubscribeButton;
---
+++ pages/atoms/Title/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Title = () => {
+  <Titlefont>Hacker News</Titlefont>;
+};
+
+const Titlefont = styled.div`
+  color: black;
+  font-size: 20px;
+  font-weigh: normal;
+`;
+
+export default Title;
